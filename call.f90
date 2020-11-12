2355af54996064b6e9cf03e6436a6ae7b1abe3a6
rtazaki1205 <ryo.tazaki1205@gmail.com>
2020-11-12 11:59:40 +0100

    set RCCA model as a default parameter set

--- call.f90
+++ call.f90
@@ -2,9 +2,9 @@
 !
 ! An example to call opacfractal.f90
 !
-! The default parameter set is the same as the benchmark test 
-! used in Appendix A in Tazaki & Tanaka (2018).
-!
+! The default parameter set is the same as the benchmark test used in Appendix A 
+! in Tazaki & Tanaka (2018), which is based on RCCA model.
+! 
 !--------------------------------------------------------------------------------
 program call_opacfractal
 implicit none
@@ -12,8 +12,7 @@ integer, parameter                      :: dp = selected_real_kind(P=15)
 real(kind=dp),parameter                 :: pi = 3.1415926535897932384_dp
 integer                                 :: iqsca,iqcor,iqgeo,iquiet,i,nang
 complex(kind=dp)                        :: ref
-real(kind=dp)                           :: lmd,R0,PN,DF,k0
-real(kind=dp)                           :: ang,dang
+real(kind=dp)                           :: lmd,R0,PN,DF,k0,ang,dang
 real(kind=dp)                           :: Cext,Csca,Cabsp,Gsca,dphi
 real(kind=dp),allocatable,dimension(:,:):: Smat
 !--------------------------------------------------------------------------------
@@ -21,14 +20,14 @@ real(kind=dp),allocatable,dimension(:,:):: Smat
 !--------------------------------------------------------------------------------
 iqsca  = 3                               ! a method to solve light scattering
 iqcor  = 1                               ! correlation function
-iqgeo  = 1                               ! geometric cross section
+iqgeo  = 2                               ! geometric cross section
 iquiet = 0                               ! stdout
 R0     = 0.5_dp                          ! Monomer radius (micron)
 PN     = 64.0_dp                         ! Number of monomers
 nang   = 91                              ! Angle mesh
 lmd    = 0.8_dp                          ! wavelength (micron)
 k0     = 0.825_dp                        ! fractal prefactor
-df     = 3.0_dp                          ! fractal dimension
+df     = 2.0_dp                          ! fractal dimension
 ref    = cmplx(1.4_dp,0.0001_dp,kind=dp) ! complex refractive index
 !--------------------------------------------------------------------------------
 ! RUN OPACFRACTAL
@@ -40,7 +39,7 @@ call meanscatt(lmd,R0,PN,Df,k0,ref,iqsca,iqcor,iqgeo,iquiet,nang,&
 ! OUTPUT 
 !--------------------------------------------------------------------------------
 DANG=0.5d0*PI/DBLE(NANG-1)
-open(10,file="out_smat.dat",status="unknown")
+open(10,file="out_smat_develop.dat",status="unknown")
 write(10,1100) iqsca,   " = iqsca"
 write(10,1100) iqcor,   " = iqcor"
 write(10,1100) iqgeo,   " = iqgeo"
